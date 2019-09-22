f62f77e0aae0459604e128e1635eb5bc69305285
javier perez <javierp2013@gmail.com>
2019-09-22 08:38:44 -0700

    added using statement to resolve conflict.

--- Global.cs
+++ Global.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using WagglesBot.Modules;
