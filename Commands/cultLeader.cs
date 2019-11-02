d555786163b313f7c6036a5af182ea654a576061
javier perez <javierp2013@gmail.com>
2019-11-02 10:49:11 -0700

    Added module for changing nicknames!

---
+++ Commands/cultLeader.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWaggles.Commands
+{
+    public class cultLeader : ModuleBase<SocketCommandContext>
+    {
+        [Command("nick")]
+        [RequireUserPermission(GuildPermission.ManageChannels)]
+        public async Task setNick(SocketGuildUser oldUser, [Remainder] string nick)
+        { 
+            await oldUser.ModifyAsync(user => user.Nickname = nick);
+            await ReplyAsync("Changed name to " + nick);
+        }
+    }
+}
