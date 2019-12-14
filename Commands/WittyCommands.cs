57127698f65f57749352c738bea5e8e7cc32adee
javier perez <javierp2013@gmail.com>
2019-12-13 21:34:25 -0800

    Added Wittys.JSON to store object data between restarts. Added file writing to aforementioned file Modified bot start code to include loading WittyDict from file

--- Commands/WittyCommands.cs
+++ Commands/WittyCommands.cs
@@ -1,6 +1,8 @@
 ﻿using Discord.Commands;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,36 +12,65 @@ namespace CoreWaggles.Commands
 
     public class WittyCommands : ModuleBase<SocketCommandContext>
     {
+        public readonly string path = "JSONstorage/Wittys.JSON";
         [Command("witty add")]
         public async Task addWitty(string name, string match, double probability, params String[] responses)
         {
+            //Makes sure probability, when multiplied by 100, will be valid.
+            //EX. .40 * 100 = 40% chance of happening.
             if (probability > 1.0 || probability < 0.0)
             {
                 await ReplyAsync("Pick a probability between 1.0 and 0");
                 return;
             }
-
+            //call wittyobject constructor with given values
             WittyObject temp = new WittyObject(name, match, probability, responses.ToList());
+
+            //error handling for null wittyDictionary List objects.
             if (!Global.wittyDictionary.ContainsKey(Context.Guild.Id))
             {
                 Global.wittyDictionary.Add(Context.Guild.Id, new List<WittyObject>());
             }
+            //adds witty to associated server wide dictionary
             Global.wittyDictionary[Context.Guild.Id].Add(temp);
+            //write object JSON info to file
+            string channelObject = JsonConvert.SerializeObject(Global.wittyDictionary);
+            if (File.Exists(path))
+            {
+                File.WriteAllText(path, channelObject);
+            }
+            else
+            {
+                await ReplyAsync($"Could not find file at {Directory.GetCurrentDirectory()}");
+                return;
+            }
             await ReplyAsync("Added Witty with name: " + temp.name);
         }
         [Command("witty remove")]
         public async Task RemWitty(string name)
         {
-            WittyObject temp;
+            //iterate through List of witties, compare names to given name.
             foreach (WittyObject wit in Global.wittyDictionary[Context.Guild.Id])
             {
                 if (wit.name == name)
                 {
                     Global.wittyDictionary[Context.Guild.Id].Remove(wit);
                     await ReplyAsync("removed " + name + "!");
+                    //write witties to file
+                    string channelObject = JsonConvert.SerializeObject(Global.wittyDictionary);
+                    if (File.Exists(path))
+                    {
+                        File.WriteAllText(path, channelObject);
+                    }
+                    else
+                    {
+                        await ReplyAsync($"Could not find file at {Directory.GetCurrentDirectory()}");
+                        return;
+                    }
                     return;
                 }
             }
+            //only reaches this point if entire list does not match.
             await ReplyAsync("Could not find that Witty, try '~witty list' to see see all wittys!");
         }
             
@@ -85,10 +116,12 @@ namespace CoreWaggles.Commands
             }
             await ReplyAsync("Witty by that name not found! Try ~witty list to get all witty names!");
         }
+        //more to be added later!
         [Command("wittydesc")]
         public async Task describe()
         {
-            await ReplyAsync(Global.wittyDictionary[Context.Guild.Id].First().responses.Count.ToString());
+            string response = Global.wittyDictionary[Context.Guild.Id].Count + " Wittys in my memory. \n";
+            await ReplyAsync(response);
         }
     }
 }
--- WittyObject.cs
+++ WittyObject.cs
@@ -26,17 +26,25 @@ namespace CoreWaggles
         public static void Process(SocketCommandContext context, string message)
         {
             Random rand = new Random();
+            //pick number between 0 and 100
             int prob = rand.Next(0, 100);
+            //iterate through witties, see if a wittys regex matches the message
             foreach (WittyObject wit in Global.wittyDictionary[context.Guild.Id])
             {
+                //if random number is less than probability * 100, run checks
+                // EX. prob = 37, wit.probability * 100 = 40
+                //60% chance prob is greater than, 40% chance its less than
                 if (prob < wit.probability * 100)
                 {
                      
                     Regex rx = new Regex(wit.trigger);
+                    //if message fits the regex
                     if (rx.IsMatch(message))
                     {
+                        //pick one of the registered responses!
                         int chosen = rand.Next(0, wit.responses.Count);
                         string response = wit.responses.ElementAt(chosen);
+                        //send response
                         context.Channel.SendMessageAsync(response);
                     }
                 }
