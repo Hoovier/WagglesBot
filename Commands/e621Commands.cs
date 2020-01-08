6c257e89eae4e9defdbdfa6ec2951afa0e83165a
Javier Perez <46412350+Hoovier@users.noreply.github.com>
2020-01-07 17:11:08 -0800

    E621 (#41)
    * Added E621 object and related search command. Heavy WIP to prevent content against Discord TOS within responses.
    
    * Cleaned up code, moved HTTP request to object file instead of command.
    
    * Added global cache for e621 searches, and ~en command, much like ~n for derpibooru.
    
    * Added ~e621tags, and associated helper function for formatting. some extra error handling for ~enext.
    
    * Added sorting integer overload for ~e search, along with instructions to adding manual sorting options.
    Fixed issue with default search choosing an index for imageList that does not exist.
    
    * Added Readability improvements, added index argument for ~n.
    
    * Added typing message and some aliases for ~e
    
    * Fixed error with sorting integer going into search string
    
    * Fixed filter.

---
+++ Commands/e621Commands.cs
@@ -0,0 +1,177 @@
+﻿using Discord.Commands;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ImageList = System.Collections.Generic.List<CoreWaggles.e621.Image>;
+
+
+namespace CoreWaggles.Commands
+{
+    public class e621Commands : ModuleBase<SocketCommandContext>
+    {
+        //array for sorting options, https://e621.net/help/show/cheatsheet#sorting
+        //-id is default, newest first, id is reverse
+        public readonly string[] sortingOptions = { "-id", "id", "score", "favcount", "random" };
+
+        [Command("e")]
+        [Alias("e621", "e6")]
+        public async Task e621SearchSort(int sort, [Remainder]string srch)
+        {
+            await Context.Channel.TriggerTypingAsync();
+            if(srch.Contains("loli") || srch.Contains("foalcon") || srch.Contains("cub"))
+            {
+                await ReplyAsync("Those search terms(loli, foalcon, or cub) are not allowed! Try again!");
+                return;
+            }
+            //if sort integer is too small or big, give help
+            if (sort - 1 > sortingOptions.Length || sort < 0)
+            {
+                await ReplyAsync("Sorting integers are: \n```0:Default, newest to oldest. \n1:Oldest to newest. \n2:Score. \n3:FavCount. \n4:Random.```" +
+                    "\nManual sorting can be done by using this chart https://e621.net/help/show/cheatsheet#sorting and inserting it into a search as a tag. " +
+                    "\nEx: Overriding default search order with mpixels order.```~e order:mpixels horse```");
+                return;
+            }
+            //url for use, explicit images and inserts provided tags straight in.
+            string url;
+            //if channel is not on the explicit channels list,
+            if (!Global.safeChannels.ContainsKey(Context.Channel.Id) && !Context.IsPrivate)
+            {
+                url = $"https://e621.net/post/index.json?tags=order:{sortingOptions[sort]}+{srch}+rating:s&limit=50";
+            }
+            else
+            {
+                url = $"https://e621.net/post/index.json?tags=order:{sortingOptions[sort]}+{srch}+-cub+-loli+-foalcon&limit=50";
+            }
+            string respond = e621.getJSON(url).Result;
+            if (respond == "failure")
+            {
+                await ReplyAsync("An error occurred!");
+                return;
+            }
+            ImageList responseList = JsonConvert.DeserializeObject<ImageList>(respond);
+            if (responseList.Count == 0)
+                await ReplyAsync("No results! The tag may be misspelled, or the results could be filtered out due to channel!");
+            else
+            {
+                Global.e621Searches[Context.Channel.Id] = respond;
+                Random rand = new Random();
+                Global.e621SearchIndex = rand.Next(0, responseList.Count);
+                e621.Image chosenImage = responseList[Global.e621SearchIndex];
+                await ReplyAsync(chosenImage.file_url + "\n" + string.Join(",", chosenImage.artist));
+            }
+        }
+        [Command("e")]
+        [Alias("e621", "e6")]
+        public async Task e621Search([Remainder] string srch)
+        {
+            //run same command as sorted one, but pass default option as arg
+            await e621SearchSort(0, srch);
+        }
+        [Command("en")]
+        [Alias("enext", "ne")]
+        public async Task e621Next()
+        {
+            await Context.Channel.TriggerTypingAsync();
+            if (Global.e621Searches.ContainsKey(Context.Channel.Id))
+            {
+                ImageList responseList = JsonConvert.DeserializeObject<ImageList>(Global.e621Searches[Context.Channel.Id]);
+                if (responseList.Count == 0)
+                {
+                    await ReplyAsync("No results! The tag may be misspelled, or the results could be filtered out due to channel!");
+                    return;
+                }
+                if (responseList.Count == 1)
+                {
+                    await ReplyAsync("Only one result to show! \n" + responseList.ElementAt(0));
+                    return;
+                }
+                if (responseList.Count == Global.e621SearchIndex)
+                    Global.e621SearchIndex = 0;
+                Global.e621SearchIndex++;
+                await ReplyAsync(responseList.ElementAt(Global.e621SearchIndex).file_url);
+            }
+            else
+            {
+                await ReplyAsync("You have to make a search first! Try running ~e <tag(s)>");
+            }
+        }
+        [Command("en")]
+        [Alias("enext", "ne")]
+        public async Task e621NextSpecific(int index)
+        {
+            await Context.Channel.TriggerTypingAsync();
+            if (Global.e621Searches.ContainsKey(Context.Channel.Id))
+            {
+                ImageList responseList = JsonConvert.DeserializeObject<ImageList>(Global.e621Searches[Context.Channel.Id]);
+                if (responseList.Count == 0)
+                {
+                    await ReplyAsync("No results! The tag may be misspelled, or the results could be filtered out due to channel!");
+                    return;
+                }
+                if (responseList.Count == 1)
+                {
+                    await ReplyAsync("Only one result to show! \n" + responseList.ElementAt(0));
+                    return;
+                }
+                if (responseList.Count < index)
+                {
+                    await ReplyAsync("Thats too big, choose a number between 0-" + (responseList.Count - 1));
+                    return;
+                }
+                Global.e621SearchIndex = index + 1;
+                await ReplyAsync(responseList.ElementAt(Global.e621SearchIndex).file_url);
+            }
+            else
+            {
+                await ReplyAsync("You have to make a search first! Try running ~e <tag(s)>");
+            }
+        }
+
+        [Command("etags")]
+        [Alias("et", "e621tags")]
+        public async Task e621Tags()
+        {
+            await Context.Channel.TriggerTypingAsync();
+            if (Global.e621Searches.ContainsKey(Context.Channel.Id))
+            {
+                ImageList responseList = JsonConvert.DeserializeObject<ImageList>(Global.e621Searches[Context.Channel.Id]);
+                e621.Image chosen = responseList.ElementAt(Global.e621SearchIndex);
+                if (responseList.Count == 0)
+                {
+                    await ReplyAsync("No results! The tag may be misspelled, or the results could be filtered out due to channel!");
+                    return;
+                }
+
+                await ReplyAsync(e621Helper.Builde621Tags(chosen));
+            }
+            else
+            {
+                await ReplyAsync("You have to make a search first! Try running ~e <tag(s)>");
+            }
+        }
+    }
+     
+    public class e621Helper
+    {
+        //This dictionary returns the string corresponding to the rating member in an Image object.
+        //ex: ratings[s] returns "Safe,"
+        public readonly static Dictionary<string, string> ratings = new Dictionary<string, string>() 
+        { { "s", "Safe, " }, { "q", "Questionable, " }, { "e", "Explicit, " } };
+
+        /// <summary>Takes an e621.Image object and returns formatted tag string.</summary>
+        /// <param name="img">An e621.Image object that will have tags extracted</param>
+        /// <returns>Formatted tag and artist string.</returns>
+        public static string Builde621Tags(e621.Image img)
+        {
+            // Adds commas to tags, for easier reading.
+            string tagString = img.tags.Replace(" ", ", ");
+            // Create string with ratings and artist tags
+            string artistAndRating = ratings[img.rating.ToString()] + "**Artist(s):** " + string.Join(",", img.artist);
+
+            //return string with lots of formatting!
+            return "**Info:** " + artistAndRating + "\n\n" + "All tags: \n```" + tagString + "```";
+        }
+    }
+}
---
+++ e621.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreWaggles
+{
+    public class e621
+    {
+        public class CreatedAt
+        {
+            public string json_class { get; set; }
+            public int s { get; set; }
+            public int n { get; set; }
+        }
+        public class Image
+        {
+            public string id { get; set; }
+            public string tags { get; set; }
+            public string locked_tags { get; set; }
+            public string description { get; set; }
+            public CreatedAt created_at { get; set; }
+            public string creator_id { get; set; }
+            public string author { get; set; }
+            public string change { get; set; }
+            public string source { get; set; }
+            public int score { get; set; }
+            public int fav_count { get; set; }
+            public string md5 { get; set; }
+            public int file_size { get; set; }
+            public string file_url { get; set; }
+            public string file_ext { get; set; }
+            public string preview_url { get; set; }
+            public int preview_width { get; set; }
+            public int preview_height { get; set; }
+            public string sample_url { get; set; }
+            public int sample_width { get; set; }
+            public int sample_height { get; set; }
+            public char rating { get; set; }
+            public string status { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+            public bool has_comments { get; set; }
+            public bool has_notes { get; set; }
+            public bool has_children { get; set; }
+            public string children { get; set; }
+            public string parent_id { get; set; }
+            public string[] artist { get; set; }
+            public string[] sources { get; set; }
+        }
+        public static async Task<string> getJSON(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //userAgent info
+                string info = "WagglesBot/1.0 (by Hoovier)";
+                string type = "application/json";
+                client.BaseAddress = new Uri(url);
+
+                //random client things, not super sure if all of it is needed apart from UserAgent stuff.
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.UserAgent.Clear();
+                //important useragent things, not allowed through without this
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(info);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(type));
+                HttpResponseMessage response = await client.GetAsync(String.Empty);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
+                else
+                {
+                    return "failure";
+                }
+            }
+        }
+    }
+}
