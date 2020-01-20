e9ef2e0e0f5503ba8ca873eb1aa519b7317bce43
javier perez <javierp2013@gmail.com>
2020-01-19 23:06:05 -0800

    Aded compatibility with multiple channels at same time, and typing free reddit search navigation.

---
+++ RedditHelper.cs
@@ -0,0 +1,33 @@
+﻿using Discord.Commands;
+using Discord.Rest;
+using Reddit;
+using Reddit.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreWaggles
+{
+    class RedditHelper
+    {
+        public int index;
+        public RestUserMessage redditMessageToTrack;
+        public ulong redditIDtoTrack;
+        public SocketCommandContext redContext;
+        public List<Post> postDictionary = new List<Post>();
+        public int getAmount()
+        {
+            return postDictionary.Count;
+        }
+        public Post getChosenPost()
+        {
+            return postDictionary[index];
+        }
+        public void setTrackingData(ulong id, RestUserMessage msg, SocketCommandContext context)
+        {
+            this.redditIDtoTrack = id;
+            this.redditMessageToTrack = msg;
+            this.redContext = context;
+        }
+    }
+}
