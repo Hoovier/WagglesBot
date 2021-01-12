d7e946e09780c0444d4d296df29dd11d66da3f6e
javier perez <javierp2013@gmail.com>
2021-01-11 22:32:58 -0800

    Fixed users being able to give themselves any role, added functionality to take role when user removes reaction, and tests for permissions.

--- Commands/reactionRoles.cs
+++ Commands/reactionRoles.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +11,39 @@ namespace CoreWaggles.Commands
     public class reactionRoles : ModuleBase<SocketCommandContext>
     {
         [Command("addrole")]
+        [RequireUserPermission(ChannelPermission.ManageRoles)]
         public async Task addRole(Discord.IRole role, string emoji, ulong messageID)
         {
-            //add a task to list
-            DBTransaction.setReactionRole(role.Id, Context.Guild.Id, emoji, messageID);
-            await ReplyAsync("Success! Reacting with ``" + emoji + "`` will give the user the ``" + role.Name + "`` role!");
+            //try to give user role in order to test if perms are present
+            IGuildUser user = (IGuildUser)Context.Message.Author;
+            try
+            {
+                //if user has it, remove then add back, this prevents removal of roles from people who already had a role
+                if(user.RoleIds.Contains(role.Id))
+                {
+                    await user.RemoveRoleAsync(role);
+                    await user.AddRoleAsync(role);
+                }
+                //if they dont, add it and then remove again
+                else
+                {
+                    await user.AddRoleAsync(role);
+                    await user.RemoveRoleAsync(role);
+                }
+                Console.WriteLine("Successful Role addition test. Permissions ok.");
+                //add a task to list
+                DBTransaction.setReactionRole(role.Id, Context.Guild.Id, emoji, messageID);
+                await ReplyAsync("Success! Reacting with ``" + emoji + "`` will give the user the ``" + role.Name + "`` role!");
+            }
+            catch
+            {
+                await ReplyAsync("Sorry! Something went wrong! Make sure that I have permission to modify roles and that my role is higher than the selected one!");
+            }
+            
         }
 
         [Command("removerole")]
+        [RequireUserPermission(ChannelPermission.ManageRoles)]
         public async Task removeRole(Discord.IRole role)
         {
             //remove a task from the database!
