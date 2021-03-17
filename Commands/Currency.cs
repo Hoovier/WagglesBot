b7dfa59756321648c635f31573cbf16f8062af6a
Javier Perez <46412350+Hoovier@users.noreply.github.com>
2021-03-16 21:38:23 -0700

    Changed ~bet to either gain or lose amount betted, instead of half. (#75)
    * Changed ~bet to either gain or lose amount betted, instead of half.
    Added ~slots to allow users to play slots
    
    * changed bonus cooldown to 1 hour instead of 8, and added leaderboard command.
    
    * Added basic lootbox skeleton and associated testing functions. Subject to change.
    
    * fixed lower bound for lootbox prizes

--- Commands/Currency.cs
+++ Commands/Currency.cs
@@ -15,7 +15,7 @@ namespace CoreWaggles.Commands
         [Alias("daily", "gimme", "giv", "gibplz")]
         public async Task DailyAsync()
         {
-            if(Context.IsPrivate)
+            if (Context.IsPrivate)
             {
                 await ReplyAsync("Please run this command within the server of your choice!");
                 return;
@@ -33,8 +33,8 @@ namespace CoreWaggles.Commands
             //figure out how long its been since the last ~daily
             DateTime stamp = DateTime.ParseExact(timestamp, "yyyy-MM-dd.HH:mm:ss", CultureInfo.InvariantCulture);
             TimeSpan span = localDate - stamp;
-            
-            if(span.TotalHours > 8)
+
+            if (span.TotalHours > 1)
             {
                 DBTransaction.giveMoney(Context.User.Id, 100, Context.Guild.Id, dateString);
                 string bal = DBTransaction.getMoneyBalance(Context.User.Id, Context.Guild.Id);
@@ -42,8 +42,8 @@ namespace CoreWaggles.Commands
             }
             else
             {
-                double remaining = Math.Round(480 - span.TotalMinutes);
-                if(remaining < 61)
+                double remaining = Math.Round(60 - span.TotalMinutes);
+                if (remaining < 61)
                 {
                     await ReplyAsync("Sorry, wait " + remaining.ToString() + " Minutes!");
                 }
@@ -69,7 +69,7 @@ namespace CoreWaggles.Commands
                 return;
             }
             string bal = DBTransaction.getMoneyBalance(Context.User.Id, Context.Guild.Id);
-            if(bal == "NONE")
+            if (bal == "NONE")
             {
                 await ReplyAsync("You are poor and destitute, not a cent to your name! Use ~bonus to beg for some.");
             }
@@ -79,6 +79,7 @@ namespace CoreWaggles.Commands
             }
         }
         [Command("balance")]
+        [Alias("bal")]
         public async Task BalanceAsync(SocketGuildUser user)
         {
             if (Context.IsPrivate)
@@ -98,6 +99,20 @@ namespace CoreWaggles.Commands
             }
         }
 
+        [Command("leaders")]
+        [Alias("leaderboard")]
+        public async Task MoneyLeadersAsync()
+        {
+            if (Context.IsPrivate)
+            {
+                await ReplyAsync("Sorry, this command is for server use only.");
+                return;
+            }
+            string leaderString = DBTransaction.getMoneyLeaders(Context.Guild.Id);
+
+            await ReplyAsync(leaderString);
+        }
+
         [Command("pay")]
         public async Task PayAsync(SocketGuildUser user, int amount)
         {
@@ -107,12 +122,12 @@ namespace CoreWaggles.Commands
                 return;
             }
             int senderBal = int.Parse(balanceString);
-            if(amount > senderBal || amount <= 0)
+            if (amount > senderBal || amount <= 0)
             {
                 await ReplyAsync("Sorry, your balance of " + senderBal + " Bits is too low!");
                 return;
             }
-            if(DBTransaction.payMoney(user.Id, amount, Context.Guild.Id) == 1)
+            if (DBTransaction.payMoney(user.Id, amount, Context.Guild.Id) == 1)
             {
                 DBTransaction.payMoney(Context.User.Id, -amount, Context.Guild.Id);
                 await ReplyAsync("Payment succesful! Your balance is now " + (senderBal - amount) + " Bits!");
@@ -127,7 +142,7 @@ namespace CoreWaggles.Commands
                 await ReplyAsync("You dont have any money to bet!");
                 return;
             }
-            if (amount < 1)
+            if (amount < 2)
             {
                 await ReplyAsync("You have to bet more than that! Cheapskate.");
                 return;
@@ -144,15 +159,10 @@ namespace CoreWaggles.Commands
             //give it a 40% chance to succeed!
             if (chosenNum < 41)
             {
-                //this makes sure that no matter how low they bet they will always get a prize if they win.
-                if(amount == 1)
-                {
-                    amount = 2;
-                }
-                rowsAffected = DBTransaction.payMoney(Context.User.Id, amount / 2, Context.Guild.Id);
+                rowsAffected = DBTransaction.payMoney(Context.User.Id, amount, Context.Guild.Id);
                 if (rowsAffected == 1)
                 {
-                    await ReplyAsync("Congrats! You won " + (amount / 2) + " Bits, bringing your balance to " + (int.Parse(balanceString) + (amount / 2)) + "Bits!");
+                    await ReplyAsync("Congrats! You won " + amount + " Bits, bringing your balance to " + (int.Parse(balanceString) + amount) + "Bits!");
                 }
                 else
                 {
@@ -161,17 +171,151 @@ namespace CoreWaggles.Commands
             }
             else
             {
-                rowsAffected = DBTransaction.payMoney(Context.User.Id, -(amount / 2), Context.Guild.Id);
+                rowsAffected = DBTransaction.payMoney(Context.User.Id, -amount, Context.Guild.Id);
                 if (rowsAffected == 1)
                 {
-                    await ReplyAsync("Oof. You lost " + (amount / 2) + " Bits, bringing your balance to " + (int.Parse(balanceString) - (amount / 2)) + "Bits!");
+                    await ReplyAsync("Oof. You lost " + amount + " Bits, bringing your balance to " + (int.Parse(balanceString) - amount) + "Bits!");
                 }
                 else
                 {
                     await ReplyAsync("An error occurred! Contact Hoovier!");
                 }
             }
+        }
+
+        [Command("slots")]
+        public async Task playSlots(int amount)
+        {
+            //check if user has the bits
+            string balanceString = DBTransaction.getMoneyBalance(Context.User.Id, Context.Guild.Id);
+            if (balanceString == "NONE" || balanceString == "0")
+            {
+                await ReplyAsync("You dont have any money to bet!");
+                return;
+            }
+            if (amount < 2)
+            {
+                await ReplyAsync("You have to bet more than that! Cheapskate.");
+                return;
+            }
+            int senderBal = int.Parse(balanceString);
+            if (amount > senderBal || amount <= 0)
+            {
+                await ReplyAsync("Sorry, your balance of " + senderBal + " Bits is too low!");
+                return;
+            }
+            //hard codes the emojis that will correspond with every random number
+            Dictionary<int, string> emojiDic = new Dictionary<int, string>
+            {
+                {0, "<:momoderp:670375029741060103>" },
+                {1, "<:stare:785151828114931732>" },
+                {2, "<:rymnut:644961603619651584>" },
+                {3, ":star:" },
+                {4, "<:FloDelet:767858750761467904>" }
+            };
+            int rowsAffected;
+            List<int> chosenNumbers = new List<int>();
+            Random rand = new Random();
+            string response = "";
+            for (int i = 0; i < 3; i++)
+            {
+                //gets a random number and stores it in the list
+                chosenNumbers.Add(rand.Next(5));
+                //gets random number generated from list.
+                response += emojiDic[chosenNumbers[i]] + " ";
+            }
+            //all match
+            if (chosenNumbers[0] == chosenNumbers[1] && chosenNumbers[2] == chosenNumbers[1])
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBTransaction.payMoney(Context.User.Id, amount * 3, Context.Guild.Id);
+                await ReplyAsync("3 matches! You win " + (amount * 3) + " Bits, bringing your balance to " + (int.Parse(balanceString) + (amount * 3)) + "Bits!");
+                return;
+            }
+            // if 0 matches 1, if 1 matches 2, or 0 matches 2, but not all three
+            else if (chosenNumbers[0] == chosenNumbers[1] || chosenNumbers[2] == chosenNumbers[1] || chosenNumbers[0] == chosenNumbers[2])
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBTransaction.payMoney(Context.User.Id, amount, Context.Guild.Id);
+                await ReplyAsync("2 matches! You win " + amount + " Bits, bringing your balance to " + (int.Parse(balanceString) + amount) + "Bits!");
+                return;
+            }
+            else
+            {
+                await ReplyAsync(response);
+                rowsAffected = DBTransaction.payMoney(Context.User.Id, -amount, Context.Guild.Id);
+                await ReplyAsync("Whoops, you lost " + amount + "Bits, bringing your balance to " + (int.Parse(balanceString) - amount) + "Bits!");
+            }
+        }
+
+        [Command("lootbox")]
+        [Alias("lb")]
+
+        public async Task lootbox()
+        {
+            await ReplyAsync("Purchasable Lootboxes:\n**Stone:** 100 Bits```0-200 Bit Prizes```**Iron:** 250 Bits\n```100-400 Bit Prizes```**Gold**: 1000 Bits\n```250-2000 Bit Prizes```");
+        }
+
+        [Command("lootbox")]
+        [Alias("lb")]
+        public async Task lootbox(string option)
+        {
+            //check if user has the bits
+            string balanceString = DBTransaction.getMoneyBalance(Context.User.Id, Context.Guild.Id);
+            if (balanceString == "NONE" || balanceString == "0")
+            {
+                await ReplyAsync("You dont have any money!");
+                return;
+            }
+            int senderBal = int.Parse(balanceString);
+            if ((100 > senderBal && option == "stone") || (250 > senderBal && option == "iron") || (1000 > senderBal && option == "gold"))
+            {
+                await ReplyAsync("Sorry, your balance of " + senderBal + " Bits is too low!");
+                return;
+            }
+            Random rand = new Random();
+            int prize = 0;
+            if(option == "stone")
+            {
+                prize = rand.Next(200);
+                await ReplyAsync("You paid 100 Bits for a Stone Lootbox and got " + prize + "Bits bringing your balance to " + (senderBal - 100 + prize));
+                DBTransaction.payMoney(Context.User.Id, prize-100, Context.Guild.Id);
+            }
+            else if (option == "iron")
+            {
+                prize = rand.Next(100,400); 
+                await ReplyAsync("You paid 250 Bits for a Iron Lootbox and got " + prize + "Bits bringing your balance to " + (senderBal - 250 + prize));
+                DBTransaction.payMoney(Context.User.Id, prize - 250, Context.Guild.Id);
+            }
+            else if (option == "gold")
+            {
+                prize = rand.Next(250,2000);
+                await ReplyAsync("You paid 1000 Bits for a Gold Lootbox and got " + prize + "Bits bringing your balance to " + (senderBal - 1000 + prize));
+                DBTransaction.payMoney(Context.User.Id, prize - 1000, Context.Guild.Id);
+            }
+            else
+            {
+                await ReplyAsync("Sorry, you can choose Stone, Iron, or Gold lootboxes only!");
+            }
+        }
+
+        [Command("lootboxtest")]
+        [Alias("lbtest")]
+        public async Task lootboxSim(int stoneMin, int stoneMax, int ironMin, int IronMax, int goldMin, int goldMax, int iterations)
+        {
+            Random rand = new Random();
+            int stonetotal = 0, irontotal = 0, goldtotal = 0;
+
+            for (int i = 0; i < iterations; i++)
+            {
+                stonetotal = stonetotal + rand.Next(stoneMin, stoneMax);
+                irontotal = irontotal + rand.Next(ironMin, IronMax);
+                goldtotal = goldtotal + rand.Next(goldMin, goldMax);
+            }
+            await ReplyAsync($"**Stone:** {stoneMin}-{stoneMax}\n**Iron:** {ironMin}-{IronMax}\n**Gold:** {goldMin}-{goldMax}\n**{iterations} iterations run:**\n**StoneSpent:** {iterations * 100} " +
+                $"**TotalStone Winnings:** {stonetotal} **StoneAVG:** {stonetotal / iterations}\n**IronSpent:** {iterations * 250} **TotalIron Winnings:** {irontotal} **IronAVG:** {irontotal / iterations}\n" +
+                $"**GoldSpent:** {iterations * 1000} **TotalGold Winnings:** {goldtotal} **GoldAVG:** {goldtotal / iterations}");
+        }
 
         }
-    }
 }
